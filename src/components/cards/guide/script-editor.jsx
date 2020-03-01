0e2810804544cd6c38afa89ccea33baa3d54fa52
ismail <ismail@MacBook-Air-de-ismail.local>
2020-03-01 16:31:30 +0100

    guide-new-version

--- src/components/cards/guide/script-editor.jsx
+++ src/components/cards/guide/script-editor.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from "react";
 import Editor from "react-medium-editor";
+import { Input } from "reactstrap";
 
 // load theme styles with webpack
 require("medium-editor/dist/css/medium-editor.css");
@@ -9,7 +10,9 @@ export default class ScriptEditor extends React.Component {
     super();
     this.state = {
       text: "Give this guide a title",
-      contenu: ""
+      contenu: "Enter a question or a prompt",
+      values: [],
+      questions: [{ question: "" }]
     };
     this.handleChangeTitle = this.handleChangeTitle.bind(this);
     this.handleChangeContenu = this.handleChangeContenu.bind(this);
@@ -18,9 +21,51 @@ export default class ScriptEditor extends React.Component {
   handleChangeTitle(text, medium) {
     this.setState({ text: text });
   }
-  handleChangeContenu(contenu, medium) {
+  handleChangeContenu(contenu) {
     this.setState({ contenu: contenu });
   }
+
+  handleChange(i, event) {
+    let values = [...this.state.values];
+    console.log(values);
+    values[i] = event.target.value;
+    this.setState({ values });
+  }
+
+  addClick() {
+    this.setState(prevState => ({
+      questions: [...prevState.questions, { question: "" }]
+    }));
+    console.log(this.state.questions);
+  }
+  createUI() {
+    return this.state.questions.map((el, i) => (
+      <div className="inlineBtn-left">
+        <div id="btn-add" onClick={this.addClick.bind(this)}>
+          +
+        </div>
+        <Editor
+          key={i}
+          id="prompt"
+          tag="pre"
+          text={this.state.contenu}
+          value={el.question || ""}
+          name="question"
+          onChange={this.handleChange.bind(this, i)}
+          options={{
+            toolbar: { buttons: ["bold", "italic", "underline"] }
+          }}
+        />
+      </div>
+    ));
+  }
+
+  handleChange(i, event) {
+    let questions = [...this.state.questions];
+    questions[i] = event;
+    this.setState({ questions });
+  }
+
   render() {
     return (
       <Fragment>
@@ -34,15 +79,7 @@ export default class ScriptEditor extends React.Component {
               toolbar: { buttons: ["bold", "italic", "underline"] }
             }}
           />
-          <Editor
-            id="card-title"
-            tag="pre"
-            text={"Inter a prompt or a question"}
-            onChange={this.handleChangeTitle}
-            options={{
-              toolbar: { buttons: ["bold", "italic", "underline"] }
-            }}
-          />
+          {this.createUI()}
         </div>
       </Fragment>
     );
