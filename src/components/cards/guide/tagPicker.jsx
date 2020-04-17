2e0379353ab53faf9097e864bddaad11bf5fe500
ismailkherbach <gi_kherbach@esi.dz>
2020-04-17 17:22:09 +0100

    tag-update

--- src/components/cards/guide/tagPicker.jsx
+++ src/components/cards/guide/tagPicker.jsx
@@ -1,10 +1,19 @@
 import React, { Fragment } from "react";
+import { Button } from "reactstrap";
 
 const TagPicker = ({ children }) => {
   return (
     <Fragment>
       <div id="tags-card">
-        <h5 id="card-title">Guides Sorted by tags</h5>
+        <h5 id="card-title">Add your tags</h5>
+        <div className="inlineBtn-col-center">
+          <input
+            className="tag-input-large"
+            placeholder="Write your tags here and hit enter to save them"
+            type="text"
+          />
+          <Button className="save-changes">Save changes</Button>
+        </div>
       </div>
     </Fragment>
   );
