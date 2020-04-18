274ada8561b5b7b8c7ad1a30fac4351b2d603b60
ismailkherbach <gi_kherbach@esi.dz>
2020-04-18 15:58:42 +0100

    calll-section-design

--- src/components/cards/call/ScriptCard.jsx
+++ src/components/cards/call/ScriptCard.jsx
@@ -1,22 +1,31 @@
 import React, { Fragment } from "react";
 import Btn from "../../small.componenets/Btn";
 import { Button } from "reactstrap";
-const ScriptCard = ({ children }) => {
-  return (
-    <Fragment>
-      <div id="calls-card">
-        <div className="inlineBtn-left">
-          <Button className="btn-unclicked">
-            <box-icon name="notepad" type="solid" color="#254ebe"></box-icon>
-            <h3 className="btn-text-unclicked">Choose a guide</h3>
-          </Button>
-          <Button className="btn-clicked">
-            <box-icon name="note" type="solid" color="#8BA3FF"></box-icon>
-            <h3 className="btn-text-clicked">Continue without guide</h3>
-          </Button>
+class ScriptCard extends React.Component {
+  render() {
+    return (
+      <Fragment>
+        <div id="calls-card">
+          <div className="inlineBtn-left">
+            <Button className="btn-unclicked">
+              <box-icon name="notepad" type="solid" color="#254ebe"></box-icon>
+              <h3 className="btn-text-unclicked">Choose a guide</h3>
+            </Button>
+            <Button className="btn-clicked">
+              <box-icon name="note" type="solid" color="#8BA3FF"></box-icon>
+              <h3 className="btn-text-clicked">Continue without guide</h3>
+            </Button>
+          </div>
+          <div className="notes inlineBtn-center">
+            <input
+              className="notes-text-area"
+              placeholder="Notes"
+              type="text"
+            />
+          </div>{" "}
         </div>
-      </div>
-    </Fragment>
-  );
-};
+      </Fragment>
+    );
+  }
+}
 export default ScriptCard;
