7ce08b516a45bc4f389b8e7cfcea8be216f9f3bb
ismailkherbach <gi_kherbach@esi.dz>
2020-07-01 14:33:19 +0100

    fixes

--- server.js
+++ server.js
@@ -1,20 +1,18 @@
-const express = require("express");
-const http = require("http");
-const path = require("path");
-const wakeDyno = require("woke-dyno");
+const express = require('express');
+const http = require('http');
+const path = require('path');
 
 const app = express();
+const wakeDyno = require("woke-dyno");
+const DYNO_URL = process.env.DYNO_URL;
+app.use(express.static(path.join(__dirname, 'build')));
+app.all('/*', (req, res) => res.sendFile(path.join(__dirname, '/build/index.html')));
 
-app.listen(PORT, () => {
-  wakeDyno("https://afternoon-thicket-26736.herokuapp.com").start(); // DYNO_URL should be the url of your Heroku app
-});
-app.use(express.static(path.join(__dirname, "build")));
-app.all("/*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/build/index.html"))
-);
-
-const port = process.env.PORT || "8080";
-app.set("port", port);
+const port = process.env.PORT || '8080';
+app.set('port', port);
 
 const server = http.createServer(app);
-server.listen(port, () => console.log(`Running on localhost:${port}`));
+server.listen(port, () => {
+    console.log(Running on localhost:${port})
+    wakeDyno(DYNO_URL).start();
+});
