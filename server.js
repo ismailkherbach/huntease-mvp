5aa8d7ced07dd88ee32cbac7c4ae4f000bee2add
ismailkherbach <gi_kherbach@esi.dz>
2020-07-01 14:26:58 +0100

    fixes

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const http = require("http");
 const path = require("path");
+const wakeDyno = require("woke-dyno");
 
 const app = express();
 
+app.listen(PORT, () => {
+  wakeDyno("https://afternoon-thicket-26736.herokuapp.com").start(); // DYNO_URL should be the url of your Heroku app
+});
 app.use(express.static(path.join(__dirname, "build")));
 app.all("/*", (req, res) =>
   res.sendFile(path.join(__dirname, "/build/index.html"))
