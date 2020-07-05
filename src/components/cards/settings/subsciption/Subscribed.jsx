835d1bbf483997b632ee1c4bcc05d777e9148c6b
ismailkherbach <gi_kherbach@esi.dz>
2020-07-05 17:50:33 +0100

    fixes

--- src/components/cards/settings/subsciption/Subscribed.jsx
+++ src/components/cards/settings/subsciption/Subscribed.jsx
@@ -36,6 +36,7 @@ class Subscribed extends React.Component {
       changeAddress: false,
       changeCard: false,
       cancelSubscription: false,
+      billing: true,
     };
   }
 
@@ -77,6 +78,11 @@ class Subscribed extends React.Component {
     });
     this.props.shouldBlur();
   }
+  toggleBilling() {
+    this.setState({
+      billing: !this.state.billing,
+    });
+  }
   async loadStripe() {
     const stripePromise = await loadStripe(
       "pk_test_51GqdyRBXLsKUPQbHXGJsCSA9tJYHPpXDa8Y8dChs4dW20yeQh3HT55oiMNmysRhogzBHWKSHvfCWr5DF9KlkKyfk00CQF8DBeX"
@@ -111,7 +117,6 @@ class Subscribed extends React.Component {
             closePopup={this.togglePopup.bind(this)}
           />
         ) : null}
-
         {this.state.cancelSubscription ? (
           <CancelSubscriptionPopup
             text='Click "Close Button" to hide popup'
@@ -124,73 +129,91 @@ class Subscribed extends React.Component {
             <h2>Huntease Growth</h2>
           </div>
           <div className="rightTop floatRight flex aic jcc fdr">
-            <Button className="Change-profile-btn">Plan</Button>
-            <Button className="Change-profile-btn btnBorder">Usage</Button>
+            <Button
+              className={`Change-profile-btn  ${
+                !this.state.billing ? "active" : ""
+              }`}
+              onClick={this.toggleBilling.bind(this)}
+            >
+              Plan
+            </Button>
+            <Button
+              className={`Change-profile-btn btnBorder ${
+                this.state.billing ? "active" : ""
+              }`}
+              onClick={this.toggleBilling.bind(this)}
+            >
+              Usage
+            </Button>
           </div>
         </div>
-        <div className="Billing-History">
-          <div className="flex fdr aic jcfs">
-            <div className="leftBottom">
-              <h4>Billing</h4>
-              <h3 className="margin-top30">
-                Your next payment will be due on<b> Jan 25th, 2021</b>
-              </h3>
-            </div>
-            <div className="rightBottom ">
-              <Button
-                className="Change-profile-btn flex aic jcc"
-                onClick={this.togglePopup.bind(this)}
-              >
-                Change address
-              </Button>
-            </div>{" "}
-          </div>
-          <div className="flex fdr aic jcfs">
-            <div className="margin-top35 margin-bottom35 leftBottom">
-              <h4>Payment Information</h4>
-              <div className="flex fdr aic">
-                <img
-                  src={require("../../../../assets/img/billing_master.png")}
-                />
-                <h3 className="padding-left10 padding-right30">
-                  {this.props.payment.cardInfo
-                    ? this.props.payment.cardInfo.brand +
-                      " ending with ****" +
-                      this.props.payment.cardInfo.last4
-                    : ""}
+        {this.state.billing ? (
+          <div className="Billing-History">
+            <div className="flex fdr aic jcfs">
+              <div className="leftBottom">
+                <h4>Billing</h4>
+                <h3 className="margin-top30">
+                  Your next payment will be due on<b> Jan 25th, 2021</b>
                 </h3>
-                <img src={require("../../../../assets/img/bxs-lock.svg")} />
+              </div>
+              <div className="rightBottom ">
+                <Button
+                  className="Change-profile-btn flex aic jcc"
+                  onClick={this.togglePopup.bind(this)}
+                >
+                  Change address
+                </Button>
+              </div>{" "}
+            </div>
+            <div className="flex fdr aic jcfs">
+              <div className="margin-top35 margin-bottom35 leftBottom">
+                <h4>Payment Information</h4>
+                <div className="flex fdr aic">
+                  <img
+                    src={require("../../../../assets/img/billing_master.png")}
+                  />
+                  <h3 className="padding-left10 padding-right30">
+                    {this.props.payment.cardInfo
+                      ? this.props.payment.cardInfo.brand +
+                        " ending with ****" +
+                        this.props.payment.cardInfo.last4
+                      : ""}
+                  </h3>
+                  <img src={require("../../../../assets/img/bxs-lock.svg")} />
+                </div>
+              </div>
+              <div className="rightBottom flex fdc aic jcc">
+                <Button
+                  className="Change-profile-btn flex aic jcc"
+                  onClick={this.togglePopupCard.bind(this)}
+                >
+                  Change payment
+                </Button>
+                <p
+                  onClick={this.togglePopupCancel.bind(this)}
+                  className="curs_pointer"
+                >
+                  Cancel Subscription
+                </p>{" "}
               </div>
             </div>
-            <div className="rightBottom flex fdc aic jcc">
-              <Button
-                className="Change-profile-btn flex aic jcc"
-                onClick={this.togglePopupCard.bind(this)}
-              >
-                Change payment
-              </Button>
-              <p
-                onClick={this.togglePopupCancel.bind(this)}
-                className="curs_pointer"
-              >
-                Cancel Subscription
-              </p>{" "}
+            <div className="scd-bloc fdr ">
+              <div className="left ">
+                <h4>Billing History</h4>
+              </div>
+            </div>
+            <div className="History-Table">
+              <table>
+                <tbody>
+                  <tr>{this.renderTableHeader()}</tr>
+                  {this.renderTableData()}
+                </tbody>
+              </table>
             </div>
           </div>
-          <div className="scd-bloc fdr ">
-            <div className="left ">
-              <h4>Billing History</h4>
-            </div>
-          </div>
-          <div className="History-Table">
-            <table>
-              <tbody>
-                <tr>{this.renderTableHeader()}</tr>
-                {this.renderTableData()}
-              </tbody>
-            </table>
-          </div>
-        </div>
+        ) : (
+          ""
+        )}{" "}
       </Fragment>
     );
   }
